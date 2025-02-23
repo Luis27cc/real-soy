d4a2d62762a238a931aebb3458a547ba3b597c24
Luis27cc <luiscampodel@gmail.com>
2025-02-23 04:18:13 +0100

    Create Recursos.js

---
+++ src/pages/Recursos.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+function Recursos() {
+  return (
+    <div className="page-container recursos-container">
+      <h1>Recursos</h1>
+      <p>Encuentra materiales útiles sobre autoestima.</p>
+      <a href="https://drive.google.com/drive/folders/1mxcWpNSYUikFynWTRx2wt_cugMgLoMTO?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <button>Explorar Recursos</button>
+      </a>
+    </div>
+  );
+}
+
+export default Recursos;
