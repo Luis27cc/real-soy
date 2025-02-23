7691998db5bf6532c77ae20f73cb0ee95309a94e
Luis27cc <luiscampodel@gmail.com>
2025-02-23 06:12:34 +0100

    Update Recursos.js

--- src/pages/Recursos.js
+++ src/pages/Recursos.js
@@ -5,6 +5,13 @@ function Recursos() {
     <div className="page-container recursos-container">
       <h1>Recursos sobre Autoestima</h1>
       <p>Encuentra artículos, libros y videos que te ayudarán a fortalecer tu autoestima.</p>
+      <a 
+        href="https://drive.google.com/drive/folders/1mxcWpNSYUikFynWTRx2wt_cugMgLoMTO?usp=sharing" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <button>Biblioteca</button>
+      </a>
     </div>
   );
 }
